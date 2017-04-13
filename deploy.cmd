ae2373e82ff668af06c1f2f842b306a58d54c410
me10zyl <me10zyl@qq.com>
2017-04-13 23:49:31 +0800

    2017-04-13 23:49:31

--- deploy.cmd
+++ deploy.cmd
@@ -1,5 +1,9 @@
+rem C:\WINDOWS\system32\cmd.exe /k hexo g & pause > nul
 set newdate=%date:~0,4%-%date:~5,2%-%date:~8,2% %time:~0,2%:%time:~3,2%:%time:~6,2%
 git add -A 
+echo %errorlevel%
 git commit -m "%newdate%"
+
 git push -u origin master
+
 pause
