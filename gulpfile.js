612bd739e2acb47d370e8d22bfdfee21684686ab
Zeng Yilun <me10zyl@qq.com>
2017-05-03 13:30:02 +0800

    2017-05-03 13:30:02

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var exec = require('child_process').exec;
 var gulp = require('gulp');
 gulp.task('hexo', function(cb){
-  exec('hexo g', function(err){
+  exec('hexo clean && hexo g', function(err){
 	if(err) return cb(err);
 	cb();
   });	
