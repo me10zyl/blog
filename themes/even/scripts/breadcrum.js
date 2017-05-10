d07387508fb92b60b40083546fcd0ff79dc81a50
me10zyl <me10zyl@qq.com>
2017-05-10 22:51:10 +0800

    2017-05-10 22:51:09

--- themes/even/scripts/breadcrum.js
+++ themes/even/scripts/breadcrum.js
@@ -1,11 +1,14 @@
 
-hexo.extend.helper.register('breadcrum', function(path,title){
+hexo.extend.helper.register('breadcrum', function(path,title,is_draft){
      if(title == 'index.html'){ return "" };
      var res = "<a href='/'> 首页 </a> &gt ";
      var config = hexo.theme.config;
      var map = config.breadcrum;
      var t = map[title.split("/")[0]];
      if(!t){ t = title.split("/")[0] };
+	 if(is_draft){
+		res +=  "<a href='/essays'> 随笔 </a> &gt "
+	 }
      res += "<a href='"+path+"'>"+ t + "</a>";
   return res;
 });
