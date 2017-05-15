b897f11ab3262c8de6baab3f06217bbab257ee68
me10zyl <me10zyl@qq.com>
2017-05-15 08:36:34 +0800

    2017-05-15  8:36:32

--- themes/even/scripts/breadcrum.js
+++ themes/even/scripts/breadcrum.js
@@ -6,7 +6,7 @@ hexo.extend.helper.register('breadcrum', function(path,title,is_draft){
      var map = config.breadcrum;
      var t = map[title.split("/")[0]];
      if(!t){ t = title.split("/")[0] };
-	 if(is_draft){
+	 if(is_draft === false){
 		res +=  "<a href='/essays'> 随笔 </a> &gt "
 	 }
      res += "<a href='"+path+"'>"+ t + "</a>";
