835323307ca7e1cca05e3a89e751bbce89e8cf0b
zyl <me10zyl@qq.com>
2017-11-07 10:51:32 +0800

    2017-11-07 10:51:32

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var exec = require('child_process').exec;
 var gulp = require('gulp');
 gulp.task('hexo', function(cb){
-  exec('hexo clean && hexo g', function(err){
+  exec('node_modules/.bin/hexo clean && node_modules/.bin/hexo g', function(err){
 	if(err) return cb(err);
 	cb();
   });	
